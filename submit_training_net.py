c98e20c6169b169a1bb25bd2a55e6c880dcfc3ca
Yosef Tufan <Yosefi.tofan@mail.huji.ac.il>
2026-01-24 15:17:53 +0200

    Update submit_training_net.py to correct path and remove unrelevants notes

--- submit_training_net.py
+++ submit_training_net.py
@@ -12,26 +12,16 @@ from slurm_job import SlurmJob
 print("going to submit a job to run transformer encoder")
 
 job = SlurmJob(
- 
-    # job_name="training_transformer_encoder_layer5", #for the original model 
     job_name="training_transformer_encoder_layer5_david",
-    job_folder = "/ems/elsc-labs/london-m/lior.avraham1/layer5/training_net_job_output3",
+    job_folder = "/ems/elsc-labs/london-m/lior.avraham1/SingleNeuronTransformer/training_net_job_output3",
     mem = 80000,
-    
-    # run_line = """
-    # #!/bin/bash -l
-    # cd /ems/elsc-labs/london-m/lior.avraham1/layer5
-    # source .venv/bin/activate
-    # python transformer_encoder_layer5.py
-    # """, #to the original model 
     run_line = """
     #!/bin/bash -l
-    cd /ems/elsc-labs/london-m/lior.avraham1/layer5
+    cd /ems/elsc-labs/london-m/lior.avraham1/SingleNeuronTransformer
     source .venv/bin/activate
     python model.py
-    """, #to david change model 
+    """,  
     run_on_GPU=True,
-    # run_on_GPU=False,
     new_cluster=True,
     allow_ss_cluster=False
 )
